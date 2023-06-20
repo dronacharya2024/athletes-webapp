029a9ca0644aed199ae0a3ddda26bae519dc1a12
trisha-ailneni <ta2087@nyu.edu>
2023-06-20 11:24:20 +0530

    cleaned up db connection

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 
 #import necessary packages
 from flask import Flask
-#import pymongo
-from flask_pymongo import pymongo
+import db
 
 # instantiate the app
 app = Flask(__name__)
-
-@app.route('/')
-def flask_mongodb_atlas():
-    return "flask mongodb atlas!"
 
 '''
 # turn on debugging if in development mode
@@ -17,29 +12,15 @@ if config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mode
 '''
-'''
-#connect to mongodb database
-CONNECTION_STRING = "mongodb+srv://anikaroy:<8DTDLlwCsv7R9bTC>@cluster0.gelqpzw.mongodb.net/"
 
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('flask_mongodb_atlas')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
-'''
-
-import certifi
-
-cluster = pymongo.MongoClient("mongodb+srv://anikaroy:8DTDLlwCsv7R9bTC@cluster0.gelqpzw.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=certifi.where())
-db = cluster["athletes_web_app"]
-collection = db["athletes_data"]
-
-
-#import db
+@app.route('/')
+def flask_mongodb_atlas():
+    return "flask mongodb atlas!"
 
 #test to insert data to the data base
 @app.route("/test")
 def test():
-    #db.db.collection.insert_one({"name": "John"})
-    collection.insert_one({"name": "John"})
+    db.collection.insert_one({"name": "John"})
     return "Connected to the data base!"
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,11 +1,9 @@
-from flask import Flask
 from flask_pymongo import pymongo
-from app import app
 import certifi
 
 #connect to mongodb database
-CONNECTION_STRING = "mongodb+srv://anikaroy:8DTDLlwCsv7R9bTC@cluster0.gelqpzw.mongodb.net/?retryWrites=true&w=majority"
+connection_string = "mongodb+srv://anikaroy:8DTDLlwCsv7R9bTC@cluster0.gelqpzw.mongodb.net/?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
-db = client.get_database('athletes_web_app')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
+cluster = pymongo.MongoClient(connection_string, tlsCAFile=certifi.where())
+db = cluster["athletes_web_app"]
+collection = db["athletes_data"]
